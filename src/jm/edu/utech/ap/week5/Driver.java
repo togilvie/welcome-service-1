84bf0afcf700522060ad11e74eb089a661486e0c
tygilv <tiagay10@hotmail.com>
2018-09-27 14:56:43 -0500

    Made changes: Added driver

---
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -0,0 +1,14 @@
+package jm.edu.utech.ap.week5;
+
+public class Driver implements Driveable {
+
+	public void drive(float numberOfMiles) {
+		System.out.println(String.format("O'Gilvie has driven" + "%.2f miles", numberOfMiles));
+	}
+
+	
+
+
+
+	
+}
